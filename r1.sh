c270dece8187571fc116b2a6b86db989686d5178
roshantom <roshantom01@gmail.com>
2017-04-11 15:14:23 +0530

    repository created

---
+++ r1.sh
@@ -0,0 +1,22 @@
+echo
+echo 'Currently logged user and his logname'
+who am i
+echo
+echo 'Your current shell'
+echo $SHELL
+echo
+echo 'Your home directory'
+echo $HOME
+echo
+echo 'Your operating system type'
+uname -a
+echo
+echo 'Your current path setting'
+echo $PATH
+echo
+echo 'Your current working directory'
+pwd
+echo
+echo 'Show currently logged number of users'
+users
+echo
